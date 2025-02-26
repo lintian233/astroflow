163d7a258b72b256d9bd298f858fa77cec75c067
LZY <2540437977@qq.com>
2025-02-26 11:46:44 +0800

    change

--- src/core/corecal.cpp
+++ src/core/corecal.cpp
@@ -5,15 +5,15 @@ deddata dedisperse_data(Filterbank &fil, float dm_low, float dm_high,
                         int time_downsample, float t_sample) {
   switch (fil.nbits) {
   case 8:
-    return dedispered::dedispered_fil_tsample_omp<uint8_t>(
-        fil, dm_low, dm_high, freq_start, freq_end, dm_step, REF_FREQ_END,
-        time_downsample, t_sample);
+    return cpucal::dedispered_fil_omp<uint8_t>(fil, dm_low, dm_high, freq_start,
+                                               freq_end, dm_step, REF_FREQ_END,
+                                               time_downsample, t_sample);
   case 16:
-    return dedispered::dedispered_fil_tsample_omp<uint16_t>(
+    return cpucal::dedispered_fil_omp<uint16_t>(
         fil, dm_low, dm_high, freq_start, freq_end, dm_step, REF_FREQ_END,
         time_downsample, t_sample);
   case 32:
-    return dedispered::dedispered_fil_tsample_omp<uint32_t>(
+    return cpucal::dedispered_fil_omp<uint32_t>(
         fil, dm_low, dm_high, freq_start, freq_end, dm_step, REF_FREQ_END,
         time_downsample, t_sample);
   default:
